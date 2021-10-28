e85005e53075f369a7ceec8d0f3866d1d2991b82
christian.pronovost <christian.pronovost@dana.com>
2021-10-28 13:09:23 -0400

    simple change

--- flask_sspi/__init__.py
+++ flask_sspi/__init__.py
@@ -1,7 +1,6 @@
 import sys
 
-modulename = 'flask_sspi_fake'
-if modulename in sys.modules:  # once imported keep using stubs
+if 'flask_sspi_fake' in sys.modules:  # once imported keep using stubs
     from flask_sspi_fake import *
 else:
     from ._common import requires_authentication, init_sspi, authenticate, Impersonate
